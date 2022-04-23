51b6cec4d91e450e40508d299b98a41ba8f993b3
LuigiVanin <luisfvaninmartins@gmail.com>
2022-04-22 22:48:34 -0300

    fix: removing template data

--- src/data.js
+++ src/data.js
@@ -1,26 +1,4 @@
 export default {
-    users: [
-        {
-            username: "bob",
-            avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        },
-        {
-            username: "bobesponja",
-            avatar: "",
-        },
-    ],
-    tweets: [
-        {
-            username: "bob",
-            tweet: "ola",
-        },
-        {
-            username: "bob",
-            tweet: "teste",
-        },
-        {
-            username: "bob",
-            tweet: "ola",
-        },
-    ],
+    users: [],
+    tweets: [],
 };
